1bb4585eb7861d2c7e122313aeaac1f4ad0b7b40
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-11-23 18:26:26 +0530

    Basic

---
+++ Facebook Interview Coding Questions/First Occurence.java
@@ -0,0 +1,6 @@
+class Solution {
+    // Function to locate the occurrence of the string pat in the string txt.
+    int firstOccurence(String txt, String pat) {
+         return txt.indexOf(pat);
+    }
+}
